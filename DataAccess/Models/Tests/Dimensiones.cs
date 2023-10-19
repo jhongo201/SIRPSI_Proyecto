49208a87430e3518a4db95ee33225916903355e0
yeison-bf <yeison.barrios.funieles@gmail.com>
2023-10-19 14:49:05 -0500

    add_module_test

---
+++ DataAccess/Models/Tests/Dimensiones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Tests
+{
+    [Table("Dimensiones", Schema = "sirpsi")]
+    public partial class Dimensiones
+    {
+        [Key]
+        public string? Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Nombre { get; set; }
+        public string? IdEstado { get; set; }
+        public string? IdUsuarioRegistra { get; set; }
+        public string? IdDominio { get; set; }
+    }
+}
---
+++ DataAccess/Models/Tests/Dominios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Models.Tests
+{
+    [Table("Dominios", Schema = "sirpsi")]
+    public partial class Dominios 
+    {
+        [Key]
+        public string Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Nombre { get; set; }
+        public string IdEstado { get; set; }
+        public string IdUsuarioRegistra { get; set; }
+        public string Forma { get; set; }
+
+        public static implicit operator string?(Dominios? v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
